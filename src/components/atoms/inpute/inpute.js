6e1623c8cbd7eb6f4a1cee631f00115af46d8291
Wiktorkulas51 <wiktorkulas51@gmail.com>
2021-03-26 21:13:01 +0100

    inpute

---
+++ src/components/atoms/inpute/inpute.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const inpute = styled.input`
+  color: white;
+  background-color: #000;
+  width: 450px;
+  height: 50px;
+`;
+
+export default Inpute;
---
+++ src/components/atoms/inpute/inpute.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Inpute from "./inpute";
+
+export default {
+  title: "Components/paragraph",
+  component: Inpute,
+};
+
+export const Inpute = () => <Inpute>Look for your music</Inpute>;
