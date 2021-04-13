22e5567f61315df249ab3187becb8cb4b85d537b
Wiktorkulas51 <wiktorkulas51@gmail.com>
2021-04-13 19:18:35 +0200

    issue

--- src/templates/loadingTemplate.js
+++ src/templates/loadingTemplate.js
@@ -1,68 +1,80 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import MainTemplate from "./mainTemplate";
 import Paragraph from "../components/atoms/paragraph/Paragraph";
 import styled from "styled-components";
 import { theme } from "../theme/mainTheme";
 import AnimatedBox from "../components/organism/animatedBox/animatedBox";
 
-class LoadingTemplate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true, prop: "false" };
-  }
+const LoadingTemplate = () => {
+  const [hasProps, setHasProps] = useState(undefined);
+  const [hasPropsTwo, setHasPropsTwo] = useState(undefined);
+  const [hasPropsThree, setHasPropsThree] = useState(undefined);
 
-  countTime = (interval) => {
+  // countTime = (interval) => {
+  //   setInterval(() => {
+  //     if (this.state.propTwo === "true") {
+  //       this.setState({ prop: "true", propTwo: undefined });
+  //     }
+  //     if (this.state.prop === "true") {
+  //       this.setState({ propThree: "true", prop: undefined });
+  //     }
+  //     if (this.state.propThree === "true") {
+  //       this.setState({ propTwo: "true", propThree: undefined });
+  //     }
+  //     this.setState({ prop: "true" });
+  //   }, interval);
+  // };
+
+  useEffect(() => {
     setInterval(() => {
-      this.setState({ prop: "true" });
-    }, interval);
-  };
-
-  componentDidMount() {
-    this.countTime(1000);
-  }
-
-  render() {
-    const Wrapper = styled.div`
-      position: relative;
-    `;
-
-    const StyledParagraph = styled(Paragraph)`
-      font-size: ${() => theme.fontSize.xl};
-      color: ${() => theme.grayOrange};
-      position: absolute;
-      left: 100px;
-      top: 70px;
-    `;
-
-    const StyledWrapper = styled.div`
-      position: absolute;
-      left: 0;
-      top: 0;
-      transform: translate(0%, 50%);
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-      width: 100%;
-      height: 350px;
-    `;
-
-    const giveProps = (prop) => {
-      const ani = <AnimatedBox prop={prop}></AnimatedBox>;
+      setHasProps("true");
+    }, 5000);
+    return () => {
+      setHasProps(undefined);
+      setHasPropsTwo("true");
     };
+  }, [hasProps]);
 
-    return (
-      <MainTemplate>
-        <Wrapper>
-          <StyledParagraph>Wait please ...</StyledParagraph>
-          <StyledWrapper>
-            <AnimatedBox second={this.state.prop} />
-            <AnimatedBox second />
-            <AnimatedBox />
-          </StyledWrapper>
-        </Wrapper>
-      </MainTemplate>
-    );
-  }
-}
+  const Wrapper = styled.div`
+    position: relative;
+  `;
+
+  const StyledParagraph = styled(Paragraph)`
+    font-size: ${() => theme.fontSize.xl};
+    color: ${() => theme.grayOrange};
+    position: absolute;
+    left: 100px;
+    top: 70px;
+  `;
+
+  const StyledWrapper = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    transform: translate(0%, 50%);
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 350px;
+  `;
+
+  // giveProps = ()=>{
+
+  // }
+
+  return (
+    <MainTemplate>
+      <Wrapper>
+        <StyledParagraph>Wait please ...</StyledParagraph>
+        <StyledWrapper>
+          <AnimatedBox second={hasProps} />
+          <AnimatedBox second={hasPropsTwo} />
+          <AnimatedBox second={hasPropsThree} />
+        </StyledWrapper>
+      </Wrapper>
+    </MainTemplate>
+  );
+};
 
 export default LoadingTemplate;
